51275bf71db2d1aff12cc48baeba9d140486fb72
sillybean <sillybean@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-26 21:56:51 +0000

    Fixing notices
    git-svn-id: https://plugins.svn.wordpress.org/lj-xp/tags/2.3.3@704428 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ wp-lj-comments.php
@@ -0,0 +1,23 @@
+<?php
+require_once('../../../wp-blog-header.php');
+global $wpdb;
+$id = $_GET['post_id']; 
+$number=$wpdb->get_var("SELECT COUNT(*) FROM $wpdb->comments WHERE comment_post_ID = '$id' AND comment_approved = '1'");
+$font = 2;
+if($number < 10) $image_width = 8;
+else if ($number > 10 && $number < 100) $image_width = 16;
+else $image_width = 24; 
+$image_height = 12;
+
+/* Displaying image with comments number */
+
+header("Content-type: image/png");
+$img = @ImageCreate ($image_width, $image_height);
+$black = ImageColorAllocate($img, 0, 0, 0);
+$white = ImageColorAllocate($img, 255, 255, 255);
+$transparent = ImageColorAllocateAlpha($img, 255, 255, 255, 127);
+ImageFill($img, 0, 0, $transparent); // Background
+ImageString($img , $font, 1, 1, "$number", $black); // Font
+ImagePng($img);
+ImageDestroy($img);
+?>
